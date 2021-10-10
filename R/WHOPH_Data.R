ceef95bed74e6ad6d4b0f53b1cc88ac9542c705d
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-10-10 14:03:18 -0600

    Update WHOPH_Data.R

--- R/WHOPH_Data.R
+++ R/WHOPH_Data.R
@@ -1,15 +1,6 @@
 WHOPH_Data=function(Dir,Data,id,weights,strata)
 {
-source("Data_Prep.R") #DP(Dir,Data) Function
-source("Data_Cleaning.R") #DC(Output of DP) Function
-source("Pcomposition.R") #Pcomposition (Output of DC) Function
-source("Pnoactivitybyset.R") #Pnoactivitybyset(Output of Pnoactivitybyset) Function
-source("Pnotmeetingrecs.R") #Pnotmeetingrecs(Output of Pnotmeetingrecs) Function
-source("Pnovigorous.R") #Pnovigorous(Output of Pnovigorous) Function
-source("Psedentary.R") #Psedentary(Output of Psedentary) Function
-source("Psetspecific.R") #Psetspecific(Output of Psetspecific) Function
-source("Ptotal.R") #Ptotal(Output of Ptotal) Function
-source("Calculations 05.10.2021.R") #Functions for Calculations
+
 
 df=DP(Dir,Data)
 df=DC(df)
