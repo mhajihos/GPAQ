f4f7114365e3b53b0bf52f7c064f3cedbb5a8313
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-10-10 14:25:49 -0600

    Update Pnovigorous.R

--- R/Pnovigorous.R
+++ R/Pnovigorous.R
@@ -1,12 +1,12 @@
 #Pnovigorous (unweighted)
 
-Pnovigorous=function(Data)
+Pnovigorous=function(data)
 {
-Data%>%
+data%>%
    mutate(Vig = if_else(P1==1 | P10==1, "_did vigorous physical activity", "_did no vigorous physical activity", 
                          missing = "_did no vigorous physical activity")) %>% 
    mutate(Vig = factor(Vig))%>% 
    mutate(cln = if_else(P1t3cln==1 & P4t6cln==1 & P7t9cln==1 & P10t12cln==1 & P13t15cln==1, 1, 2, missing = 2)) %>% 
-   mutate(cln = replace(cln, is.na(P1) & is.na(P4) & is.na(P7) & is.na(P10) & is.na(P13), 2))
-   return(Data)
+   mutate(cln = replace(cln, is.na(P1) & is.na(P4) & is.na(P7) & is.na(P10) & is.na(P13), 2))%>%
+   return(data)
 }
