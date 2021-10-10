589ad6b3539862064463ab31f6e14740f05f1e10
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-10-10 14:25:04 -0600

    Update Pnoactivitybyset.R

--- R/Pnoactivitybyset.R
+++ R/Pnoactivitybyset.R
@@ -1,8 +1,8 @@
 #Pnoactivitybyset (unweighted)
 
-Pnoactivitybyset=function(Data)
+Pnoactivitybyset=function(data)
 {
-Data%>%
+data%>%
   mutate(work = if_else(P1==1 | P4==1, "_did work activity", "_did no work activity", 
                             missing = "_did no work activity")) %>% 
   mutate(work = factor(work)) %>% 
@@ -13,6 +13,6 @@ Data%>%
   					missing = "_did no recreation activity")) %>% 
   mutate(rec = factor(rec)) %>% 
   mutate(cln = if_else(P1t3cln==1 & P4t6cln==1 & P7t9cln==1 & P10t12cln==1 & P13t15cln==1, 1, 2, missing = 2)) %>% 
-  mutate(cln = replace(cln, is.na(P1) & is.na(P4) & is.na(P7) & is.na(P10) & is.na(P13), 2))
-  return(Data)
+  mutate(cln = replace(cln, is.na(P1) & is.na(P4) & is.na(P7) & is.na(P10) & is.na(P13), 2))%>% 
+  return(data)
 }
