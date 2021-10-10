952b31fa85c348327dc2c221a4981b16c073615b
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-10-10 14:13:07 -0600

    Update Psetspecific.R

--- R/Psetspecific.R
+++ R/Psetspecific.R
@@ -15,5 +15,6 @@ Data%>%
   mutate(Precday = (P10t12+P13t15)/7)%>% 
   mutate(cln = if_else(P1t3cln==1 & P4t6cln==1 & P7t9cln==1 & P10t12cln==1 & P13t15cln==1, 1, 2, missing = 2)) %>% 
   mutate(cln = replace(cln, is.na(P1) & is.na(P4) & is.na(P7) & is.na(P10) & is.na(P13), 2))
+ return(Data)
 
 }
