66c42f59416e7a368f06c54797c3e2eedd4b4ebd
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-10-11 13:48:01 -0600

    Update WHOPH_Data.R

--- R/WHOPH_Data.R
+++ R/WHOPH_Data.R
@@ -1,4 +1,4 @@
-WHOPH_Data=function(Dir,Data,id,weights,strata)
+WHOPH_Data=function(Dir,Data)
 {
 
 
@@ -16,7 +16,7 @@ df=subset(df, df$cln==1)
 df_valid<-subset(df, df$valid==1)
 
 #WStep1
-STEPSC<-svydesign(id=~id, weights=~weights, strata = ~strata, data=df_valid, nest=TRUE)
+STEPSC<-svydesign(id=~PSU, weights=~WStep1, strata = ~Stratum, data=df_valid, nest=TRUE)
 
 
 return(STEPSC)
