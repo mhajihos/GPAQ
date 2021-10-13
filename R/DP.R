27ccab65ade9d3dc63a9d64c928eb95053f63a83
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-10-13 11:59:11 -0600

    Update DP.R

--- R/DP.R
+++ R/DP.R
@@ -1,14 +1,6 @@
 
 DP=function(Dir,Data)
 {
-suppressMessages(suppressWarnings(library(survey)))
-suppressMessages(suppressWarnings(library(RODBC)))
-suppressMessages(suppressWarnings(library(knitr)))
-suppressMessages(suppressWarnings(library(rlist)))
-suppressMessages(suppressWarnings(library(dplyr)))
-suppressMessages(suppressWarnings(library(stringr)))	
-	
-	
 	
 setwd(Dir)
 #Reading a dataset and data preparation
@@ -44,14 +36,14 @@ if(length(which(names(my_data)=="age"))!=0)
 		{
 			my_data$valid=my_data$Valid
 			my_data$AGE=my_data$age
-			my_data$WStep1=my_data$WSTEP1
+			
 		}else{
  my_data$P1=my_data$p1;my_data$P2=my_data$p2;my_data$P3a=my_data$p3a;my_data$P3b=my_data$p3b;my_data$P4=my_data$p4;
  my_data$P5=my_data$p5;my_data$P6a=my_data$p6a;my_data$P6b=my_data$p6b;my_data$P7=my_data$p7;my_data$P8=my_data$p8;
  my_data$P9a=my_data$p9a;my_data$P9b=my_data$p9b;my_data$P10=my_data$p10;my_data$P11=my_data$p11;my_data$P12a=my_data$p12a;
  my_data$P12b=my_data$p12b;my_data$P13=my_data$p13;my_data$P14=my_data$p14;my_data$P15a=my_data$p15a;my_data$P15b=my_data$p15b;
  my_data$P16a=my_data$p16a;my_data$P16b=my_data$p16b;
- my_data$PSU=my_data$psu;my_data$WStep1=my_data$wstep1;my_data$Stratum=my_data$stratum;my_data$AGE=my_data$age
+ my_data$AGE=my_data$age
 			 }
  }
 my_data$age4y<-cut(my_data$AGE, breaks=c(18,29,44,59,69))
@@ -59,12 +51,5 @@ my_data$age5y<-cut(my_data$AGE, breaks=c(17,19,24,29,34,39,44,49,54,59,64,69))
 
 return(my_data)
 }
-
-
-
-
-
-
-
 
 
