a5887f8ffc1474f44e0b1e805a8a4a46fc433310
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-10-16 15:05:50 -0600

    Update Pnovigorous.R

--- R/Pnovigorous.R
+++ R/Pnovigorous.R
@@ -7,5 +7,7 @@ data%>%
                          missing = "_did no vigorous physical activity")) %>% 
    mutate(vig = factor(vig))%>% 
    mutate(cln_vigorous = if_else(p1t3cln==1 & p4t6cln==1 & p7t9cln==1 & p10t12cln==1 & p13t15cln==1, 1, 2, missing = 2)) %>% 
-   mutate(cln_vigorous = replace(cln_vigorous, is.na(p1) & is.na(p4) & is.na(p7) & is.na(p10) & is.na(p13), 2))
+   mutate(cln_vigorous = replace(cln_vigorous, is.na(p1) & is.na(p4) & is.na(p7) & is.na(p10) & is.na(p13), 2))%>%
+  
+  return(data)
 }
