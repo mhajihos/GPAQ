0f3048b4bd9835581c82a76f393cb96bc2fa01d4
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-11-16 15:17:04 -0700

    Update Pnotmeetingrecs.R

--- R/Pnotmeetingrecs.R
+++ R/Pnotmeetingrecs.R
@@ -10,11 +10,11 @@ data%>%
   mutate(p10t12 = ifelse(p10t12cln==1, p11*p12*8, NA)) %>% 
   mutate(p13t15 = ifelse(p13t15cln==1, p14*p15*4, NA)) %>% 
   mutate(ptotal = p1t3+p4t6+p7t9+p10t12+p13t15) %>%
-  mutate(meet = if_else(ptotal>=600, "2) meets recs", "1) doesn't meet recs", missing = "1) doesn't meet recs")) %>% 
+  mutate(meet = if_else(ptotal>=600, "meets WHO recommendations", "doesn't WHO recommendations", missing = "1) doesn't meet recs")) %>% 
   mutate(meet = factor(meet)) %>% 
   
   # UpDATED: Calculate additional >=1200 MET-minutes/week
-  mutate(mets1200 = if_else(ptotal>=1200, "2) meets recs", "1) doesn't meet recs", missing = "1) doesn't meet recs")) %>% 
+  mutate(mets1200 = if_else(ptotal>=1200, " meets recs", " doesn't meet recs", missing = "1) doesn't meet recs")) %>% 
   mutate(mets1200 = factor(mets1200))%>%
   mutate(cln_meet = if_else(p1t3cln==1 & p4t6cln==1 & p7t9cln==1 & p10t12cln==1 & p13t15cln==1, 1, 2, missing = 2)) %>% 
   mutate(cln_meet = replace(cln_meet, is.na(p1) & is.na(p4) & is.na(p7) & is.na(p10) & is.na(p13), 2))%>% 
