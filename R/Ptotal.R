22c9b917cf2950f7885819ef18f9001b99a979a5
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-11-16 19:14:01 -0700

    Update Ptotal.R

--- R/Ptotal.R
+++ R/Ptotal.R
@@ -12,14 +12,14 @@ data%>%
   mutate(p10t12 = ifelse(p10t12cln==1, p11*p12*8, NA)) %>% 
   mutate(p13t15 = ifelse(p13t15cln==1, p14*p15*4, NA)) %>% 
   mutate(ptotal = p1t3+p4t6+p7t9+p10t12+p13t15) %>% 
-  mutate(ptotalCat = ifelse((p2+p5+p8+p11+p14)>6 & ptotal>2999, " High level", NA)) %>%
-  mutate(ptotalCat = replace(ptotalCat, (p2+p11)>2 & ptotal>1499, " High level")) %>% 
-  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & ((p2+p5+p8+p11+p14)>=5) & ptotal>=600, " Moderate level")) %>% 
-  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & ((p2+p11)==3 | (p2+p11)==4) & p12>=20 & p3>=20, " Moderate level")) %>% 
-  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & p2>=3 & p11>=3 & (p12>=20 | p3>=20), " Moderate level")) %>% 
-  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & ((p2>=3 & p11<3 & p3>=20) | (p11>=3 & p2<3 & p12>=20)), " Moderate level")) %>% 
-  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & (p5+p8+p14)>=5 & ((p5*p6)+(p8*p9)+(p14*p15))>=150, " Moderate level")) %>% 
-  mutate(ptotalCat = replace(ptotalCat, p1t3cln==1 & p4t6cln==1 & p7t9cln==1 & p10t12cln==1 & p13t15cln==1 & is.na(ptotalCat), " Low Level")) %>% 
+  mutate(ptotalCat = ifelse((p2+p5+p8+p11+p14)>6 & ptotal>2999, " _High level", NA)) %>%
+  mutate(ptotalCat = replace(ptotalCat, (p2+p11)>2 & ptotal>1499, " _High level")) %>% 
+  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & ((p2+p5+p8+p11+p14)>=5) & ptotal>=600, " _Moderate level")) %>% 
+  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & ((p2+p11)==3 | (p2+p11)==4) & p12>=20 & p3>=20, " _Moderate level")) %>% 
+  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & p2>=3 & p11>=3 & (p12>=20 | p3>=20), " _Moderate level")) %>% 
+  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & ((p2>=3 & p11<3 & p3>=20) | (p11>=3 & p2<3 & p12>=20)), " _Moderate level")) %>% 
+  mutate(ptotalCat = replace(ptotalCat, is.na(ptotalCat) & (p5+p8+p14)>=5 & ((p5*p6)+(p8*p9)+(p14*p15))>=150, " _Moderate level")) %>% 
+  mutate(ptotalCat = replace(ptotalCat, p1t3cln==1 & p4t6cln==1 & p7t9cln==1 & p10t12cln==1 & p13t15cln==1 & is.na(ptotalCat), " _Low Level")) %>% 
   mutate(ptotalCat = factor(ptotalCat)) %>% 
   mutate(cln_ptotal = if_else(p1t3cln==1 & p4t6cln==1 & p7t9cln==1 & p10t12cln==1 & p13t15cln==1, 1, 2, missing = 2)) %>% 
   mutate(cln_ptotal = replace(cln_ptotal, is.na(p1) & is.na(p4) & is.na(p7) & is.na(p10) & is.na(p13), 2))%>%
