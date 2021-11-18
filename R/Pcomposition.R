e43959d24bff92be90f22ff9a0d9a280a531f3ec
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-11-18 15:03:25 -0700

    Update Pcomposition.R

--- R/Pcomposition.R
+++ R/Pcomposition.R
@@ -9,9 +9,9 @@ data%>%
   mutate(p10t12 = ifelse(p10t12cln==1, p11*p12, NA)) %>% 
   mutate(p13t15 = ifelse(p13t15cln==1, p14*p15, NA)) %>% 
   mutate(ptotal = p1t3+p4t6+p7t9+p10t12+p13t15) %>% 
-  mutate(percentwork = ((p1t3+p4t6)/ptotal)*100) %>% 
-  mutate(percenttrans = (p7t9/ptotal)*100) %>% 
-  mutate(percentrec = ((p10t12+p13t15)/ptotal)*100) %>% 
+  mutate(percentwork = ((p1t3+p4t6)/ptotal)) %>% 
+  mutate(percenttrans = (p7t9/ptotal)) %>% 
+  mutate(percentrec = ((p10t12+p13t15)/ptotal)) %>% 
   mutate(cln_composition = if_else(p1t3cln==1 & p4t6cln==1 & p7t9cln==1 & p10t12cln==1 & 
                              p13t15cln==1, 1, 2, missing = 2)) %>% 
   mutate(cln_composition = replace(cln_composition, is.na(p1) & is.na(p4) & is.na(p7) & 
