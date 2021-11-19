c6bf2ebf28af6923223e340fc6074511689fa992
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-11-18 17:28:06 -0700

    Update PtotalCat_svy_mean.R

--- R/PtotalCat_svy_mean.R
+++ R/PtotalCat_svy_mean.R
@@ -52,24 +52,24 @@ S=data.frame(S)
 
 Category=rownames(S)
 
-Estimation1=round(S[,which(substr(colnames(S),1,10)=="ptotalCat1")],4)
+Estimation1=round(S[,which(substr(colnames(S),12,15)=="High")],4)
 CI1=paste0(round(S[,which(substr(colnames(S),1,4)=="ci_l")[1]],4),"-",
 				round(S[,which(substr(colnames(S),1,4)=="ci_u")[1]],4))
 
 
-Estimation2=round(S[,which(substr(colnames(S),1,10)=="ptotalCat2")],4)
+Estimation2=round(S[,which(substr(colnames(S),12,19)=="Moderate")],4)
 CI2=paste0(round(S[,which(substr(colnames(S),1,4)=="ci_l")[2]],4),"-",
 				round(S[,which(substr(colnames(S),1,4)=="ci_u")[2]],4))
 
 
-Estimation3=round(S[,which(substr(colnames(S),1,10)=="ptotalCat3")],4)
+Estimation3=round(S[,which(substr(colnames(S),12,14)=="Low")],4)
 CI3=paste0(round(S[,which(substr(colnames(S),1,4)=="ci_l")[3]],4),"-",
 				round(S[,which(substr(colnames(S),1,4)=="ci_u")[3]],4))
 
 Res=data.frame(Category,Estimation1,CI1,Estimation2,CI2,Estimation3,CI3)
-names(Res)[c(2,3,4,5,6,7)]=c(colnames(S)[which(substr(colnames(S),1,10)=="ptotalCat1")],"95%CI_Low",
-				colnames(S)[which(substr(colnames(S),1,10)=="ptotalCat2")],"95%CI_Moderate",
-				colnames(S)[which(substr(colnames(S),1,10)=="ptotalCat3")],"95%CI_High")
+names(Res)[c(2,3,4,5,6,7)]=c(colnames(S)[which(substr(colnames(S),1,10)=="High")],"95%CI_High",
+				colnames(S)[which(substr(colnames(S),1,10)=="Moderate")],"95%CI_Moderate",
+				colnames(S)[which(substr(colnames(S),1,10)=="Low")],"95%CI_Low")
 }
 detach(Data_subset)
 return(Res)
