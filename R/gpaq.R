17bd4061159a3ccebe9b4bf0a6762cab5c106e2e
mhajihos <50219979+mhajihos@users.noreply.github.com>
2022-04-15 13:00:29 -0600

    Update gpaq.R

--- R/gpaq.R
+++ R/gpaq.R
@@ -36,13 +36,14 @@ suppressMessages(suppressWarnings(library(dplyr)))
 
 
 df=DC(Data)
-df=Pcomposition(df)
-df=Pnoactivitybyset(df)
 df=Pnotmeetingrecs(df)
+df=Ptotal(df)  
+df=Psetspecific(df)
+df=Pnoactivitybyset(df)
+df=Pcomposition(df)
 df=Pnovigorous(df)
 df=Psedentary(df)
-df=Psetspecific(df)
-df=Ptotal(df)
+
 
 df_valid=subset(df,(df$valid==1 & !is.na(df$wstep1)))
 return(df_valid)
